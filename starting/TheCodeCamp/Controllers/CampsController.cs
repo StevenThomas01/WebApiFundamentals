1527a1e44b0fd0f17a33e6f7284b7bb0eb1e0af6
Kiet Lo <klo@intelligentenvironments.comm>
2019-11-14 16:44:36 +0000

    NotFound()

--- starting/TheCodeCamp/Controllers/CampsController.cs
+++ starting/TheCodeCamp/Controllers/CampsController.cs
@@ -11,6 +11,7 @@ using TheCodeCamp.Models;
 
 namespace TheCodeCamp.Controllers
 {
+    [RoutePrefix("api/camps")]
     public class CampsController : ApiController
     {
         private readonly ICampRepository _repository;
@@ -20,6 +21,8 @@ namespace TheCodeCamp.Controllers
             _repository = repository;
             _mapper = mapper;
         }
+
+        [Route()]
         public async Task<IHttpActionResult> Get()
         {
             try
@@ -35,6 +38,24 @@ namespace TheCodeCamp.Controllers
             {
                 //TODO: Add logging
                 return InternalServerError();
+            }
+        }
+
+        [Route("{moniker}")]
+        public async Task<IHttpActionResult> Get(string moniker)
+        {
+            try
+            {
+                var result = await _repository.GetCampAsync(moniker);
+                if (result == null) return NotFound();
+
+                var mappedResult = _mapper.Map<CampModel>(result);
+
+                return Ok(mappedResult);
+            }
+            catch(Exception ex)
+            {
+                return InternalServerError(ex);
             }
         }
     }
